3d2610d62f90493952926e6980abb83b2da552d8
Gabrielle Bertocchi Sales <gabrielle_bertocchi@hotmail.com>
2023-02-02 14:34:49 +0000

    add JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,19 @@
 var frase = $(".frase").text();//posso usar jQuery ou $, .text ele acessa o texto do elemento
 var numeroPalavras = frase.split(" ").length;//split(espaços da frase) .length(mostar tamanho no console)
 var tamanhoFrase = $("#tamanho-frase");
-
 tamanhoFrase.text("900");//.text nesse caso substitui a informaçao na var (no html)
+
+var campo = $(".campo-digitacao");
+
+campo.on("input",function(){ //on() funcao de clique, function () anonima, input = sem clicar
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length -1; // /\S+/ = expressao regular nao conta todos os espaços
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+} )
 
 
 
