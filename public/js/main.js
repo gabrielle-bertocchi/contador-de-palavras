8c1102ac16ffc6098bb4b6e7da889bae1316f730
Gabrielle Bertocchi Sales <gabrielle_bertocchi@hotmail.com>
2023-02-08 15:34:44 +0000

    Update JS

--- public/js/main.js
+++ public/js/main.js
@@ -69,18 +69,47 @@ function inicializaMarcadores() {
 }
 
 
-function inserePlacar(){
+function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Gabrielle";
+    var usuario = "Douglas"
     var numPalavras = $("#contador-palavras").text();
 
-    var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                "</tr>";
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
 
-    corpoTabela.prepend(linha);
+    corpoTabela.append(linha);
 }
+
+
+function novaLinha(usuario, palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event){
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
+
+
+$(".botao-remover").click(function(event){
+    event.preventDefault();
+    $(this).parent().parent().remove();
+
+});
 
 
 function reiniciaJogo(){
